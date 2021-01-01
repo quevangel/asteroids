9cbdeb8e106831da87d2df7227befd988a72c98d
quevangel <miguel.mtz.quevedo@gmail.com>
2021-01-01 02:39:29 -0600

    Add .gitignore and stop hardcoding window size.

--- asteroids.zig
+++ asteroids.zig
@@ -5,7 +5,11 @@ const sdl = @cImport({
     @cInclude("SDL2/SDL2_framerate.h");
 });
 
-const max_asteroids = 100;
+const window_size = .{ 1200, 800 };
+const window_width = window_size[0];
+const window_height = window_size[1];
+
+const max_asteroids = 400;
 const Asteroid = struct {
     circle: Circle,
     velocity: [2]f32,
@@ -90,8 +94,8 @@ var player: struct {
     .velocity = [2]f32{ 0, 0 },
     .damaged = false,
 };
-fn playerRotate(angle: f32) void {
-    player.angle += angle;
+fn playerRotate(angle: f32, dt: f32) void {
+    player.angle += angle * dt;
     player.angle = @mod(player.angle, std.math.tau);
     if (player.angle < 0.0) player.angle += std.math.tau;
 }
@@ -136,7 +140,7 @@ fn playerRender() SdlError!void {
             }
         }
     }
-    // try renderCircleTorus(player.circle, .{ .r = color.r, .g = color.g, .b = color.b });
+    try renderCircleTorus(player.circle, .{ .r = 255, .g = 255, .b = 0 });
     if (bullet) |real_bullet| {
         try renderCircleTorus(Circle{ .center = real_bullet.position, .radius = 5 }, .{ .r = 255, .g = 255, .b = 0 });
     }
@@ -145,8 +149,8 @@ fn renderCircleTorus(circle: Circle, color: struct { r: u8, g: u8, b: u8 }) SdlE
     inline for (.{ -1, 0, 1 }) |dx| {
         inline for (.{ -1, 0, 1 }) |dy| {
             if (dx == 0 or dy == 0) {
-                var x: i16 = @floatToInt(i16, circle.center[0] + dx * 800);
-                var y: i16 = @floatToInt(i16, circle.center[1] + dy * 800);
+                var x: i16 = @floatToInt(i16, circle.center[0] + dx * window_width);
+                var y: i16 = @floatToInt(i16, circle.center[1] + dy * window_height);
                 var r: i16 = @floatToInt(i16, circle.radius);
                 if (sdl.circleRGBA(renderer, x, y, r, color.r, color.g, color.b, 255) != 0)
                     return error.SdlError;
@@ -158,10 +162,10 @@ fn renderLineTorus(start: [2]f32, end: [2]f32, color: struct { r: u8, g: u8, b: 
     inline for (.{ -1, 0, 1 }) |dx| {
         inline for (.{ -1, 0, 1 }) |dy| {
             if (dx == 0 or dy == 0) {
-                var sx: i16 = @floatToInt(i16, start[0] + dx * 800);
-                var sy: i16 = @floatToInt(i16, start[1] + dy * 800);
-                var ex: i16 = @floatToInt(i16, end[0] + dx * 800);
-                var ey: i16 = @floatToInt(i16, end[1] + dy * 800);
+                var sx: i16 = @floatToInt(i16, start[0] + dx * window_width);
+                var sy: i16 = @floatToInt(i16, start[1] + dy * window_height);
+                var ex: i16 = @floatToInt(i16, end[0] + dx * window_width);
+                var ey: i16 = @floatToInt(i16, end[1] + dy * window_height);
                 if (sdl.lineRGBA(renderer, sx, sy, ex, ey, color.r, color.g, color.b, 255) != 0)
                     return error.SdlError;
             }
@@ -175,7 +179,7 @@ fn circleSegmentTorusIntersects(c: Circle, s: Segment) bool {
                 inline for (.{ -1, 0, 1 }) |s_dx| {
                     inline for (.{ -1, 0, 1 }) |s_dy| {
                         if (s_dx == 0 or s_dy == 0) {
-                            if (intersects(Circle{ .center = add(c.center, [2]f32{ c_dx * 800, c_dy * 800 }), .radius = c.radius }, Segment{ .start = add(s.start, [2]f32{ s_dx * 800, s_dy * 800 }), .delta = s.delta }))
+                            if (intersects(Circle{ .center = add(c.center, [2]f32{ c_dx * window_width, c_dy * window_height }), .radius = c.radius }, Segment{ .start = add(s.start, [2]f32{ s_dx * window_width, s_dy * window_height }), .delta = s.delta }))
                                 return true;
                         }
                     }
@@ -192,8 +196,8 @@ fn circleTorusIntersects(c1: Circle, c2: Circle) bool {
                 inline for (.{ -1, 0, 1 }) |s_dx| {
                     inline for (.{ -1, 0, 1 }) |s_dy| {
                         if (s_dx == 0 or s_dy == 0) {
-                            var c1p = Circle{ .center = add(c1.center, [2]f32{ c_dx * 800, c_dy * 800 }), .radius = c1.radius };
-                            var c2p = Circle{ .center = add(c2.center, [2]f32{ s_dx * 800, s_dy * 800 }), .radius = c2.radius };
+                            var c1p = Circle{ .center = add(c1.center, [2]f32{ c_dx * window_width, c_dy * window_height }), .radius = c1.radius };
+                            var c2p = Circle{ .center = add(c2.center, [2]f32{ s_dx * window_width, s_dy * window_height }), .radius = c2.radius };
                             if (c1p.intersects(c2p))
                                 return true;
                         }
@@ -210,10 +214,18 @@ fn playerPush(dt: f32, mag: f32) void {
 }
 fn playerMove(dt: f32) void {
     player.circle.center = donut(add(player.circle.center, mul(dt, player.velocity)));
+    const velocity_magnitude = magnitude(player.velocity);
+    if (velocity_magnitude < 0.00001) {
+        player.velocity = [2]f32{ 0, 0 };
+    } else {
+        const friction = 100.0;
+        const friction_velocity_magnitude = std.math.max(0, velocity_magnitude - dt * friction);
+        player.velocity = mul(friction_velocity_magnitude / velocity_magnitude, player.velocity);
+    }
     if (bullet) |*real_bullet| {
         real_bullet.position = add(real_bullet.position, mul(dt, real_bullet.velocity));
-        if (real_bullet.position[0] < 0 or real_bullet.position[0] > 800 or
-            real_bullet.position[1] < 0 or real_bullet.position[1] > 800)
+        if (real_bullet.position[0] < 0 or real_bullet.position[0] > window_width or
+            real_bullet.position[1] < 0 or real_bullet.position[1] > window_height)
             bullet = null;
     }
 }
@@ -239,10 +251,10 @@ inline fn sub(a: [2]f32, b: [2]f32) [2]f32 {
 fn donut(v: [2]f32) [2]f32 {
     var w: [2]f32 = v;
     inline for (.{ 0, 1 }) |i| {
-        if (w[i] >= 800) {
-            w[i] -= 800;
+        if (w[i] >= window_size[i]) {
+            w[i] -= window_size[i];
         } else if (w[i] < 0) {
-            w[i] += 800;
+            w[i] += window_size[i];
         }
     }
     return w;
@@ -312,7 +324,7 @@ fn setup() void {
 pub fn main() SdlError!void {
     setup();
     key_state = sdl.SDL_GetKeyboardState(null);
-    window = sdl.SDL_CreateWindow("retro-redo: asteroids", sdl.SDL_WINDOWPOS_CENTERED, sdl.SDL_WINDOWPOS_CENTERED, 800, 800, sdl.SDL_WINDOW_SHOWN) orelse return error.SdlError;
+    window = sdl.SDL_CreateWindow("retro-redo: asteroids", sdl.SDL_WINDOWPOS_CENTERED, sdl.SDL_WINDOWPOS_CENTERED, window_width, window_height, sdl.SDL_WINDOW_SHOWN) orelse return error.SdlError;
     defer sdl.SDL_DestroyWindow(window);
 
     renderer = sdl.SDL_CreateRenderer(window, -1, sdl.SDL_RENDERER_ACCELERATED) orelse return error.SdlError;
@@ -320,7 +332,7 @@ pub fn main() SdlError!void {
 
     var fps_manager: sdl.FPSmanager = undefined;
     sdl.SDL_initFramerate(&fps_manager);
-    const fps = 120;
+    const fps = 200;
     if (sdl.SDL_setFramerate(&fps_manager, fps) != 0) {
         std.debug.print("SdlError: {}\n", .{@as([*:0]const u8, sdl.SDL_GetError())});
         return error.SdlError;
@@ -328,6 +340,7 @@ pub fn main() SdlError!void {
 
     while (!process_events()) {
         _ = sdl.SDL_framerateDelay(&fps_manager);
+        std.debug.print("FPS = {}\n", .{sdl.SDL_getFramerate(&fps_manager)});
         tick(1.0 / @intToFloat(f32, fps));
         try render();
     }
@@ -350,10 +363,10 @@ fn process_events() bool {
 }
 
 fn tick(dt: f32) void {
-    if (key_state.?[sdl.SDL_SCANCODE_W] != 0) playerPush(dt, 50.0);
-    if (key_state.?[sdl.SDL_SCANCODE_S] != 0) playerPush(dt, -50.0);
-    if (key_state.?[sdl.SDL_SCANCODE_D] != 0) playerRotate(dt * 4);
-    if (key_state.?[sdl.SDL_SCANCODE_A] != 0) playerRotate(-dt * 4);
+    if (key_state.?[sdl.SDL_SCANCODE_W] != 0) playerPush(dt, 500.0);
+    if (key_state.?[sdl.SDL_SCANCODE_S] != 0) playerPush(dt, -500.0);
+    if (key_state.?[sdl.SDL_SCANCODE_D] != 0) playerRotate(5, dt);
+    if (key_state.?[sdl.SDL_SCANCODE_A] != 0) playerRotate(-5, dt);
     playerMove(dt);
     player.damaged = false;
     for (asteroids) |*maybe_asteroid| {
@@ -372,8 +385,10 @@ fn tick(dt: f32) void {
 }
 
 fn render() SdlError!void {
-    if (sdl.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255) != 0) return error.SdlError;
-    if (sdl.SDL_RenderClear(renderer) != 0) return error.SdlError;
+    if (sdl.SDL_SetRenderDrawColor(renderer, 0, 0, 64, 255) != 0)
+        return error.SdlError;
+    if (sdl.SDL_RenderClear(renderer) != 0)
+        return error.SdlError;
     try playerRender();
     for (asteroids) |maybe_asteroid| {
         if (maybe_asteroid) |asteroid| {
